2b8a72838e748f07ad1a8de9aa87fa78398dba5e
Pavel Sarlov <sarlovpavel@gmail.com>
2021-11-13 16:09:34 +0200

    update practicum

---
+++ practicum/fetch_source.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget "https://learn.fmi.uni-sofia.bg/pluginfile.php/361860/mod_resource/content/1/BivariateData.html" \
+  --header 'authority: mathjax.rstudio.com' \
+  --header 'sec-ch-ua: "Microsoft Edge";v="95", "Chromium";v="95", ";Not A Brand";v="99"' \
+  --header 'dnt: 1' \
+  --header 'sec-ch-ua-mobile: ?0' \
+  --header 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53' \
+  --header 'sec-ch-ua-platform: "Windows"' \
+  --header 'accept: */*' \
+  --header 'sec-fetch-site: cross-site' \
+  --header 'sec-fetch-mode: no-cors' \
+  --header 'sec-fetch-dest: script' \
+  --header 'referer: https://learn.fmi.uni-sofia.bg/' \
+  --header 'accept-language: en-US,en;q=0.9,fr;q=0.8,bg;q=0.7,pt;q=0.6,ru;q=0.5,sk;q=0.4,mt;q=0.3' \
+  --header "cookie: MoodleSession=20851765e43278c43325a1d75faef320"
